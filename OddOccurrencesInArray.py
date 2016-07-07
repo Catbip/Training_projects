e3209f53e34477f68a20bc2a240d542226ce262d
Catbip <mioara_br92@yahoo.com>
2016-07-07 17:29:29 +0300

    Update OddOccurrencesInArray.py

--- OddOccurrencesInArray.py
+++ OddOccurrencesInArray.py
@@ -1,8 +1,9 @@
 # A non-empty zero-indexed array A consisting of N integers is given. 
 # The array contains an odd number of elements, and each element of 
 # the array can be paired with another element that has the same value, 
-# except for one element that is left unpaired. Write a function 
-# def solution(A) that, given an array A consisting of N integers 
+# except for one element that is left unpaired. 
+
+# Write a function def solution(A) that, given an array A consisting of N integers 
 # fulfilling the above conditions, returns the value of the unpaired element.
 
 def solution(A):
