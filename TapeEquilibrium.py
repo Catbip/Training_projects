73fc328345efc3fa6f8e2c74743f35734d99b4b1
Catbip <mioara_br92@yahoo.com>
2016-07-12 17:11:35 +0300

    Update TapeEquilibrium.py

--- TapeEquilibrium.py
+++ TapeEquilibrium.py
@@ -1,11 +1,4 @@
-# A non-empty zero-indexed array A consisting of N integers is given. 
-# Array A represents numbers on a tape. Any integer P, such that 0 < P < N, 
-# splits this tape into two non-empty parts. The difference between 
-# the two parts is the absolute difference between the sum of the first part 
-# and the sum of the second part. 
-
-# Write a function def solution(A) that, given a non-empty zero-indexed array A of N integers, 
-# returns the minimal difference that can be achieved.
+# https://codility.com/programmers/task/tape_equilibrium/
 
 def solution(A):
     # write your code in Python 2.7
