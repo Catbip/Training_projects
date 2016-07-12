45c1ae7dc1a3d38fad37c0cd9b76ada281c928ea
Catbip <mioara_br92@yahoo.com>
2016-07-12 17:07:40 +0300

    Update MaxCounters.py

--- MaxCounters.py
+++ MaxCounters.py
@@ -1,10 +1,4 @@
-You are given N counters, initially set to 0, and you have two possible operations on them:
-increase(X) − counter X is increased by 1, max counter − all counters are set to the maximum value of any counter.
-A non-empty zero-indexed array A of M integers is given. This array represents consecutive operations:
-if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X), if A[K] = N + 1 then operation K is max counter.
-
-Write a function def solution(N, A) that, given an integer N and a non-empty zero-indexed array A consisting of M integers, 
-returns a sequence of integers representing the values of the counters.
+# https://codility.com/programmers/task/max_counters/
 
 def solution(N, A):
     highest = 0
